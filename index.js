9042f2c8fbb4c5de08b3008b28dd9a3cb1eea64b
Spiritofthecore <cuongdepgiai1998@gmail.com>
2019-02-26 16:20:01 +0700

    commit 4

--- index.js
+++ index.js
@@ -28,7 +28,12 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-app.listen(8080);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
 
 app.get("/", function(request, response)  {
 
