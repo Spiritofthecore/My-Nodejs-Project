7a6a9d11e88c479ed14cfdf091951bc740e8adf5
Spiritofthecore <cuongdepgiai1998@gmail.com>
2019-03-03 23:15:34 +0700

    create server

--- index.js
+++ index.js
@@ -1,46 +1,12 @@
-// var express = require("express");
-//
-// var app = express();
-//
-// app.use(express.static("public"));
-//
-// app.set("view engine", "ejs");
-// app.set("views", "./views");
-//
-// app.listen(3000);
-//
-// app.get("http://nguyenhoangcuongle.azurewebsites.net/", function(request, response)  {
-//
-//     response.render("homePage");
-// });
-//
-// app.get("http://nguyenhoangcuongle.azurewebsites.net/test", function(request, response)  {
-//
-//     response.render("testPage");
-// });
-//
-var express = require("express");
+var http = require('http');
+var fs = require("fs");
 
-var app = express();
+var server = http.createServer(function(request, response) {
 
-app.use(express.static("public"));
+    response.writeHead(200, {"Content-Type": "text/html"});
+    fs.createReadStream("./default.html").pipe(response)
 
-app.set("view engine", "ejs");
-app.set("views", "./views");
-
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-
-server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
 
-app.get("/", function(request, response)  {
-
-    response.render("homePage");
-});
-
-app.get("/test", function(request, response)  {
-
-    response.render("testPage");
-});
+var port = process.env.PORT || 1337;
+server.listen(port);
