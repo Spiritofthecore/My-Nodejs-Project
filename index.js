0abab057d6a0ca2e411e11443f97038399915f56
Nguyễn Hoàng Cương Lê <hoangcuong9331@gmail.com>
2019-03-04 11:31:50 +0700

    add route

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 var http = require('http');
 var fs = require("fs");
+var express = require("express");
+var path = require("path");
+
+var app = express();
+
+//app.use(express.static("hinh_nhom.jpg"))
+
+//app.set("view engine", "ejs")
+//app.set("views", "./members/")
+
+app.get('./members/1612039/demo_web.html', function(req, res) {
+  res.send('./members/1612039/demo_web.html')
+})
 
 var server = http.createServer(function(request, response) {
 
