8dd3428067282e6a6ad6e28ffe6c6e85299ceee7
Nguyễn Hoàng Cương Lê <hoangcuong9331@gmail.com>
2019-03-12 09:32:39 +0700

    fix bugs

--- index.js
+++ index.js
@@ -11,36 +11,33 @@ server.listen(port);
 
 app.get('/1612039', function(req, res) {
   res.sendFile(__dirname + '/members/1612039/1612039.html')
-})
+});
 
-app.get('/1612048', function(req, res) {
-  res.sendFile(__dirname + '/members/1612048/1612048.html')
-})
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/default.html')
+});
 
 app.get('/1612065', function(req, res) {
   res.sendFile(__dirname + '/members/1612065/1612065.html')
-})
+});
 
 app.get('/1612048', function(req, res) {
   res.sendFile(__dirname + '/members/1612048/1612048.html')
-})
+});
 
 app.get('/hinhhoaibao', function(req, res) {
   res.sendFile(__dirname + '/members/1612039/Images/hinhhoaibao.jpg')
-
-app.get('/chau', function(req, res) {
-  res.sendFile(__dirname + '/members/1612048/Images/chau.jpg')
-})
+});
 
 app.get('/hinhhoangcuong', function(req, res) {
   res.sendFile(__dirname + '/members/1612065/images/hinhhoangcuong.jpg')
-})
+});
 
 app.get('/hinhvanchau', function(req, res) {
   res.sendFile(__dirname + '/members/1612048/images/hinhvanchau.jpg')
-})
+});
 
 app.get('/hinhnhom', function(req, res) {
   res.sendFile(__dirname + '/hinhnhom.jpg')
-})
+});
 app.use('/CSS', express.static(__dirname + '/CSS'));
